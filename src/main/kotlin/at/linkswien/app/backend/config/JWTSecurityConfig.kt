c5f9ee7f3494872f9db93fc4ff68b73a8123c70b
Konrad Pozniak <k.pozniak@gmx.at>
2022-04-16 11:10:13 +0200

    add cors config allowing localhost:8080 and 8081 requests

--- src/main/kotlin/at/linkswien/app/backend/config/JWTSecurityConfig.kt
+++ src/main/kotlin/at/linkswien/app/backend/config/JWTSecurityConfig.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @EnableWebFluxSecurity
 @Configuration
@@ -19,8 +22,20 @@ class JWTSecurityConfig {
             .anyExchange().authenticated()
             .and()
             .csrf { x -> x.disable()}
+            .cors().configurationSource(createCorsConfigSource())
+            .and()
             .oauth2ResourceServer()
             .jwt()
         return http.build()
+    }
+
+    private fun createCorsConfigSource(): CorsConfigurationSource {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("http://localhost:8080", "http://localhost:8081")
+        config.allowedMethods = listOf("*")
+
+        source.registerCorsConfiguration("/**", config)
+        return source
     }
 }
